8c86abe8eb4e51c004c7c8d3617f7889f1ddc94c
Lucas Paludo <lucaspaludo2018@gmail.com>
2025-07-14 11:50:02 -0300

    criando estrutura html

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import { Heading } from './components/Heading';
 import './styles/theme.css'
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
 
 export function App() {
 
 
-  return (
-    <>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+  return <>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error,
-        voluptatem sed aperiam optio temporibus sequi dolorum sapiente voluptas
-        facilis harum eos maxime ipsam, asperiores, vitae nisi! In, dolorem
-        fugit. Fugit.
-      </p>
-    </>
-  );
+    <div className='container'>
+      <div className='content'>
+        <section>
+          LOGO
+        </section>
+
+      </div>
+
+    </div>
+    <div className='container'>
+      <div className='content'>
+        <section>
+          MENU
+        </section>
+
+      </div>
+
+    </div>
+    <div className='container'>
+      <div className='content'>
+        <section>
+          FORM
+        </section>
+
+      </div>
+
+    </div>
+    <div className='container'>
+      <div className='content'>
+        <section>
+          FOOTER
+        </section>
+
+      </div>
+
+    </div>
+
+  
+  
+  </>;
 }
