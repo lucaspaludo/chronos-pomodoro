5fce8b77302805e02a003dcedf4e5ace97fd3868
Lucas Paludo <lucaspaludo2018@gmail.com>
2025-07-23 11:26:09 -0300

    criando form da home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Container } from "./components/Container";
+import { CountDown } from "./components/CountDown";
 import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
@@ -10,10 +11,37 @@ export function App() {
     <>
       <Container>
         <Logo />
-        
       </Container>
+      
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className="form" action="">
+
+          <div className="formRow">
+            <label htmlFor="input">task</label> 
+            <input id="input" type="text" />
+          </div>
+
+          <div className="formRow">
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className="formRow">
+            <p>Ciclos</p>
+            <p>0 0 0 0 0 0 0</p>
+          </div>
+
+          <div className="formRow">
+           <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
